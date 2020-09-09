c13307eabffb0e65bb670d85cfbaf303d36a3fac
Vicky Nguyen <44889544+VickyNguyen99@users.noreply.github.com>
2020-09-08 19:11:48 -0700

    fix download_video() and compress(). To do: fix compress()

--- yt_slicer.py
+++ yt_slicer.py
@@ -5,65 +5,18 @@ import shlex
 from pytube import YouTube
 from subprocess import check_call, PIPE, Popen
 
-# NOTE: split then compress each of the clips (another way is to compress the video then split it, smaller clips may be less than 10MB ??)
-
-# FIXME
 def download_video(url, filename = 'blank'):
     yt = YouTube(url)
-
-    video = yt.streams.filter(res='720p', type='video', subtype='mp4', fps=30, progressive=True) \
-        or yt.streams.filter(res='1080p', type='video', subtype='mp4', fps=30, progressive=True) \
+    video = yt.streams.filter(res='1080p', type='video', subtype='mp4', fps=30, progressive=True) \
+        or yt.streams.filter(res='720p', type='video', subtype='mp4', fps=30, progressive=True) \
         or yt.streams.filter(res='1440p', type='video', subtype='mp4', fps=30, progressive=True) \
         or yt.streams.filter(res='2160p', type='video', subtype='mp4', fps=30, progressive=True)
     
     if not video:
+        print("Choose another video!")
+        return None
 
-        # download video and audio, then merge them
-        video = yt.streams.filter(res='1080p', type='video', subtype='webm', fps=30, progressive=False) \
-            or yt.streams.filter(res='720p', type='video', subtype='webm', fps=30, progressive=False) \
-            or yt.streams.filter(res='1440p', type='video', subtype='webm', fps=30, progressive=False) \
-            or yt.streams.filter(res='2160p', type='video', subtype='webm', fps=30, progressive=False) \
-            or yt.streams.filter(res='1080p', type='video', subtype='mp4', fps=30, progressive=False) \
-            or yt.streams.filter(res='720p', type='video', subtype='mp4', fps=30, progressive=False) \
-            or yt.streams.filter(res='1440p', type='video', subtype='mp4', fps=30, progressive=False) \
-            or yt.streams.filter(res='2160p', type='video', subtype='mp4', fps=30, progressive=False)
-        
-        audio = yt.streams.filter(type='audio', subtype='mp4', abr='128kbps', progressive=False) \
-            or yt.streams.filter(type='audio', subtype='mp4', abr='160kbps', progressive=False) \
-            or yt.streams.filter(type='audio', subtype='webm', abr='128kbps', progressive=False) \
-            or yt.streams.filter(type='audio', subtype='webm', abr='160kbps', progressive=False) \
-            or yt.streams.filter(type='audio', subtype='mp4', abr='70kbps', progressive=False) \
-            or yt.streams.filter(type='audio', subtype='mp4', abr='50kbps', progressive=False) \
-            or yt.streams.filter(type='audio', subtype='webm', abr='70kbps', progressive=False) \
-            or yt.streams.filter(type='audio', subtype='webm', abr='50kbps', progressive=False)
-        
-        if not video or not audio:
-            print('Choose another video')
-            return None
-
-        video[0].download(filename = 'videotmp')
-        audio[0].download(filename = 'audiotmp')
-
-        grep_video_cmd = "ls |grep 'videotmp'"
-        proc = subprocess.Popen(grep_video_cmd, stdout=subprocess.PIPE, shell=True)
-        video_name = str(proc.stdout.read().decode('ascii')).strip()
-
-        grep_audio_cmd = "ls |grep 'audiotmp'"
-        proc = subprocess.Popen(grep_audio_cmd, stdout=subprocess.PIPE, shell=True)
-        audio_name = str(proc.stdout.read().decode('ascii')).strip()
-
-        if audio_name == 'audiotmp.webm':
-            convert_cmd = 'ffmpeg -i audiotmp.webm audiotmp.mp4'
-            proc = subprocess.Popen(convert_cmd.split())
-
-        merge_cmd = 'ffmpeg -i {} -i audiotmp.mp4 -c:v copy -c:a copy {}.mp4'.format(video_name, filename)
-        subprocess.Popen(merge_cmd.split(), stdout=subprocess.PIPE)
-
-        subprocess.Popen(['rm', video_name])
-        subprocess.Popen(['rm', audio_name])
-    
-    else:
-        video[0].download(filename = filename)
+    video[0].download(filename = filename)
 
     return 1
 
@@ -116,48 +69,41 @@ def split_segment(filename, n, by='size'):
 
 def get_video_info(filename):
 
-    # size (MB in decimal)
-    cmd = 'ffprobe -i {} -show_entries format=size -v quiet'.format(filename)
+    # size (MB in decimal), bitrate
+    cmd = 'ffprobe -i {} -show_entries format=size,bit_rate -v quiet'.format(filename)
     proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     output = str(proc.stdout.read()).split('\\n')
     size = int(list(filter(lambda x: x.startswith('size='), output))[0][len('size='):]) / (10**6)
+    bitrate = int(list(filter(lambda x: x.startswith('bit_rate='), output))[0][len('bit_rate='):]) // (10**3)
 
     # width x height
     cmd = 'ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 {}'.format(filename)
     proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     dimensions = str(proc.stdout.read().decode('ascii')).strip().split('x')
 
-    # return size, width, height
-    return size, int(dimensions[0]), int(dimensions[1])
+    # return size, bitrate, width, height
+    return size, bitrate, int(dimensions[0]), int(dimensions[1])
 
 # FIXME
-def compress(filename):
+def compress(filename, bitrate):
     tmp_name = filename[: -len('.mp4')] + 'tmp.mp4'
     rename_cmd = 'mv {} {}'.format(filename, tmp_name)
-    subprocess.Popen(rename_cmd.split())
+    subprocess.Popen(rename_cmd.split(), stdout=subprocess.PIPE)
 
-    compress_cmd = 'ffmpeg -i {} -vcodec h264 -acodec aac {}'.format(tmp_name, filename)
-    subprocess.Popen(compress_cmd.split())
+    compress_cmd = 'ffmpeg -i {} -b {} {}'.format(tmp_name, bitrate, filename)
+    subprocess.Popen(compress_cmd.split(), stdout=subprocess.PIPE)
 
     subprocess.Popen(['rm', tmp_name])
 
-# NOTE: tmp function to download video. Delete this after fixing download_video()
-def get_video(url, filename):
-    yt = YouTube(url)
-    video = yt.streams.filter(res='720p', type='video', subtype='mp4') or yt.streams.filter(res='1080p', type='video', subtype='mp4')  
-    if not video:
-        print("Choose another video!")
-        return None
-    video[0].download(filename = filename)
-    return 1
-
 # test
-video = get_video('https://youtu.be/nsZObkD1dog', 'test')
+video = download_video('https://youtu.be/nsZObkD1dog', 'test')
 if video:
-    split_count = split_segment('test.mp4', 60)
+    split_count = split_segment('test.mp4', 30)
     for i in range(split_count):
-        size, width, height = get_video_info('test-{}.mp4'.format(i))
-        # print(size, width, height)
+        size, bitrate, width, height = get_video_info('test-{}.mp4'.format(i))
+        # print(size, bitrate, width, height)
 
-        # if size > 10:
-        #     compress('test-{}.mp4'.format(i))
+        # NOTE: not sure how to calculate bitrate, so just reduce 100k until size <= 10MB
+        while size > 10:
+            compress('test-{}.mp4'.format(i), str(bitrate - 100) + 'k')
+            size, bitrate, width, height = get_video_info('test-{}.mp4'.format(i))
